9c147c2aade636dcc392c132eee8348a3f128da6
Doug Hawkinson <doug5solas@gmail.com>
2017-08-31 16:47:33 -0700

    tracking 404 error

--- app.js
+++ app.js
@@ -32,7 +32,7 @@
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'pug');
 
-    app.use('static', express.static(path.join(__dirname, 'public')));
+    app.use('/static', express.static(path.join(__dirname, 'public')));
     
     //  use the routes
     app.use('/', homeRte);
